7735fce373cf87514af3c00d73195e4ded53aedf
ddilans <ddilans@github>
2021-01-29 02:15:57 -0800

    updated to today

---
+++ .vimrc
@@ -0,0 +1,5 @@
+set nobackup
+set nowritebackup
+set noswapfile
+set noundofile
+colorscheme zellner
--- lemonbar.sh
+++ lemonbar.sh
@@ -2,6 +2,7 @@
 while true; do
 echo -n "%{l}$(bat.sh)%$(charging.sh) "
 echo -n "%{c} $(date +%R) $(listhidden.sh)"
-echo "%{r}%{A1:mpc toggle:}$(mpc current)%{A}"
+echo "%{r}%{A1:mpc toggle:}%{A3:mpc next:}$(mpc current)%{A}%{A}"
 sleep 2;
-done | lemonbar -g 1920x16+1920+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
+done | lemonbar -g 1881x16+1941+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
+#done | lemonbar -g 1920x16+0+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
