2ef4950740e347834ecde515e9631e4058b96332
ddilans <ddilans@github>
2021-02-01 00:59:19 -0800

    dual bars

--- lemonbar.sh
+++ lemonbar.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 while true; do
-echo -n "%{l}$(bat.sh)%$(charging.sh) "
-echo -n "%{c} $(date +%R) $(listhidden.sh)"
-echo "%{r}%{A1:mpc toggle:}%{A3:mpc next:}$(mpc current)%{A}%{A}"
-sleep 2;
+echo -n "%{l}$(xtitle)"
+echo -n "%{c}$(bspc query -D -d .focused --names) $(bspc query -D -d .occupied --names | tr '\n' , | head -c-1) $(listhidden.sh)"
+echo "%{r}$(date +'%a %D %R'), $(TZ='Europe/Berlin' date +%R), $(TZ='Europe/Riga' date +%R)"
+sleep 1;
 done | lemonbar -g 1881x16+1941+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
-#done | lemonbar -g 1920x16+0+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
+#done | lemonbar -g 1881x16+22+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
