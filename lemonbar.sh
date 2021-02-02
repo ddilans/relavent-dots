716a1cf8a8218647ae2e8b32a63b4d7ad2e64312
ddilans <ddilans@github>
2021-02-01 21:38:34 -0800

    script modularity

--- lemonbar.sh
+++ lemonbar.sh
@@ -2,7 +2,7 @@
 while true; do
 echo -n "%{l}$(xtitle)"
 echo -n "%{c}$(bspc query -D -d .focused --names) $(bspc query -D -d .occupied --names | tr '\n' , | head -c-1) $(listhidden.sh)"
-echo "%{r}$(date +'%a %D %R'), $(TZ='Europe/Berlin' date +%R), $(TZ='Europe/Riga' date +%R)"
+echo "%{r}$(date +'%a %d/%m/%y %R'), $(TZ='Europe/Berlin' date +%R), $(TZ='Europe/Riga' date +%R)"
 sleep 1;
-done | lemonbar -g 1881x16+1941+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
+done | lemonbar -g $1 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
 #done | lemonbar -g 1881x16+22+0 -u 0 -B "#31363b" -F "#eff0f1" -f 'Misc Fixed:style=SemiCondensed:pixelsize=13:lang=ru'  -f 'aquafont:pixelsize=13' | sh
